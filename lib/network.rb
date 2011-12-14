e74eeda8f394c5124250801f7ad9bab40c919ebe
Tracey Eubanks <traceyeubanks@yahoo.com>
2011-12-13 23:36:59 -0700

    fixed network stats for ubuntu

--- lib/network.rb
+++ lib/network.rb
@@ -3,7 +3,8 @@ class Network
     network_data = {packets: {}}
     raw_stats = `netstat -s`
     ip_packets_received = raw_stats.scan(/ip:[\s]+([\d]+) total packets received/i)[0][0].to_i
-    ip_packets_delivered = raw_stats.scan(/(?:([\d]+) total packets delivered|([\d]+) packets sent from this host)/)[0][1].to_i
+    ip_packets_delivered = raw_stats.scan(/([\d]+) requests sent out/)[0][0].to_i
+    ip_packets_delivered = raw_stats.scan(/([\d]+) packets sent from this host/)[0][1].to_i if(ip_packets_delivered == 0)
     network_data[:packets][:received] = ip_packets_received
     network_data[:packets][:delivered] = ip_packets_delivered
     network_data
